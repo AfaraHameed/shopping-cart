a3d46786e0ddb7dada3e5a0a1d63859e75278ec5
Afara <afarasidheek@gmail.com>
2021-09-06 05:17:00 +0400

    get the products from data base and displayed

--- routes/admin.js
+++ routes/admin.js
@@ -4,33 +4,11 @@ var productHelper= require('../helpers/product-helpers');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  let products=[
-    {
-      name:"Aagyeyi Fabulous Kurtis",
-      category:"Kurtis",
-      Description:"Fabric:Rayon\nSleeve Length:Three-Quarter Sleeves\nPattern:Embroidered",
-      image:"https://images.meesho.com/images/products/42511545/dzye1_512.jpg"
-    },
-    {
-      name:"Bbanita Ensemble Kurtis",
-      category:"Kurtis",
-      Description:"Fabric:Cotton \nSleeve Length:Three-Quarter Sleeves",
-      image:"https://images.meesho.com/images/products/42232997/ujudp_512.jpg"
-    },
-    {
-      name:"C kurtis",
-      category:"Kurtis",
-      Description:"Fabric:Rayon\nSleeve Length:Three-Quarter Sleeves\nPattern:Printed",
-      image:"https://images.meesho.com/images/products/42691244/hhu3a_512.jpg"
-    },
-    {
-      name:"Adrika Alluring Kurtis",
-      category:"Kurtis",
-      Description:"Fabric:Cotton\nSleeve Length:Three-Quarter Sleeves\nPattern:Printed",
-      image:"https://images.meesho.com/images/products/42511545/dzye1_512.jpg"
-    }
-  ]
-  res.render('admin/view-products',{admin:true,products})
+  productHelper.getAllProducts().then((product)=>{
+    console.log(product)
+    res.render('admin/view-products',{admin:true,product})
+  })
+  
 });
 router.get('/add-product',function(req,res){
    res.render('admin/add-product')
